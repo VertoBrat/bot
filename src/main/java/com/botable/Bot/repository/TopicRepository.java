cc5b5228d1b19cd072217422080d63a34aa19384
merechko.dmitriy <merechko.dmitriy@otr.ru>
2025-12-03 16:24:26 +0300

    create repos

---
+++ src/main/java/com/botable/Bot/repository/TopicRepository.java
@@ -0,0 +1,10 @@
+package com.botable.Bot.repository;
+
+import com.botable.Bot.domain.Topic;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TopicRepository extends JpaRepository<Topic, Long> {
+    Optional<Topic> findByName(String name);
+}
