5450f0b35ea23f961a02e4091aca1804d12c2e58
merechko.dmitriy <merechko.dmitriy@otr.ru>
2025-12-05 01:02:13 +0300

    gpt tune

--- src/main/java/com/botable/Bot/service/BotService.java
+++ src/main/java/com/botable/Bot/service/BotService.java
@@ -1,7 +1,5 @@
 package com.botable.Bot.service;
 
-import com.botable.Bot.domain.Message;
-import com.botable.Bot.repository.TopicRepository;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -9,7 +7,6 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
-import java.util.Random;
 
 @Service
 @RequiredArgsConstructor
@@ -21,19 +18,10 @@ public class BotService {
     @Setter
     @Getter
     private Long chartId;
-    private final TopicRepository topicRepository;
     private final ChatGptService chatGptService;
 
     public Optional<String> handleIncomingText(String text) {
         return Optional.ofNullable(chatGptService.generate(text));
-
-
-        /*return Optional.ofNullable(text)
-                .map(String::toLowerCase)
-                .flatMap(topicRepository::findMatchingTopic)
-                .flatMap(topic -> Optional.ofNullable(topic.getMessages())
-                        .flatMap(messages -> Optional.ofNullable(messages.get(new Random().nextInt(messages.size())))))
-                .map(Message::getContent);*/
     }
 
 }
